a60564609d32bf04196e2332115e74e1ee79208c
Selier <selier@163.com>
2023-10-23 16:22:30 +0800

    update file

--- temp/auto-commit.sh
+++ temp/auto-commit.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+
+# 设置代理
+export http_proxy=http://192.168.5.83:7890
+export https_proxy=http://192.168.5.83:7890
+
 cd ~/Project/hello-idea/temp/
 # 获取当前时间
 strftime=$(date +"%Y-%m-%d %H:%M:%S")
 
-# 打开原始文件并读取内容
-lines=$(cat temp.py)
-
-# 在第5行后添加新行
+# 添加新行
 new_line="    print(\"$strftime\")"
-
-# 将修改后的内容写回原始文件
 echo "$new_line" >> temp.py
-
+echo "文件修改成功"
 # 执行git commit
 commit_message="$strftime i commit code"
 git commit -am "$commit_message"
+echo "文件提交中"
+git push
+echo "文件提交完成✅"
