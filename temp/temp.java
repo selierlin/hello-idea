5630d67a270f55390418fdaaca7a40ce9cb8f12e
Selier <selier@163.com>
2023-10-23 15:38:44 +0800

    2023-10-23 15:38:44 i commit code

--- temp/temp.java
+++ temp/temp.java
@@ -3,6 +3,7 @@ package temp;
 public class temp {
     public static void main(String[] args) {
 
+        System.out.println("2023-10-23 15:38:44");
         System.out.println("2023-10-23 15:38:43");
         System.out.println("2023-10-23 15:38:40");
     }
